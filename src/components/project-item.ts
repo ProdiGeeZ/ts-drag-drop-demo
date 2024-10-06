054190383214ef055db17bfcd87024b071030e40
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-06 19:03:40 +0100

    organise files into folders to make code more modular and readable

---
+++ src/components/project-item.ts
@@ -0,0 +1,48 @@
+// * Project Item Class
+/// <reference path="./base-component.ts"/>
+/// <reference path="../decorators/autobind.ts"/>
+/// <reference path="../models/project.ts"/>
+/// <reference path="../models/drag-drop.ts"/>
+/// <reference path="../state/project-state.ts"/>
+namespace App {
+    export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+        private project: Project;
+
+        get persons() {
+            if (this.project.people === 1) {
+                return '1 person';
+            } else {
+                return `${this.project.people.toString()} people`;
+            }
+        }
+
+        constructor(hostId: string, project: Project) {
+            super('single-project', hostId, false, project.id);
+            this.project = project;
+
+            this.configure();
+            this.renderContent();
+        }
+
+        @autobind
+        dragStartHandler(event: DragEvent) {
+            event.dataTransfer!.setData('text/plain', this.project.id);
+            event.dataTransfer!.effectAllowed = 'move';
+        }
+
+        dragEndHandler(event: DragEvent) {
+            console.log(event, 'dragEnd');
+        }
+
+        configure() {
+            this.element.addEventListener('dragstart', this.dragStartHandler);
+            this.element.addEventListener('dragend', this.dragEndHandler);
+        }
+
+        renderContent() {
+            this.element.querySelector('h2')!.textContent = this.project.title;
+            this.element.querySelector('h3')!.textContent = this.persons + ' assigned.';
+            this.element.querySelector('p')!.textContent = this.project.description;
+        }
+    }
+}
