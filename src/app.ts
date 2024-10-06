5c7c8bd133717b3882ee78ea34e77e2ad92715d7
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-06 16:39:16 +0100

    add a method to change the project status and rerender result based on the list it is dragged to.

--- src/app.ts
+++ src/app.ts
@@ -54,6 +54,20 @@ class ProjectState extends State<Project> {
             listenerFn(this.projects.slice());
         }
     }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if (project && project.status !== newStatus) {
+            project.status = newStatus;
+        }
+        this.updateListeners();
+    }
+
+    private updateListeners() {
+        for (const listenerFn of this.listeners) {
+            listenerFn(this.projects.slice());
+        }
+    }
 }
 
 const projectState = ProjectState.getInstance();
@@ -197,8 +211,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         }
     }
 
+    @autobind
     dropHandler(event: DragEvent) {
-        console.log(event)
+        const projectId = event.dataTransfer!.getData('text/plain');
+        projectState.moveProject(projectId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
     }
 
     @autobind
